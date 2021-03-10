80445787654b832ee62d813282474c7e5beca60a
Dmitriy <dmitriy.sai2013@gmail.com>
2021-03-10 11:41:32 +0200

    fix tests

--- Tests/SwiftRepositoryTests/SwiftRepositoryTests.swift
+++ Tests/SwiftRepositoryTests/SwiftRepositoryTests.swift
@@ -2,11 +2,8 @@ import XCTest
 @testable import SwiftRepository
 
 final class SwiftRepositoryTests: XCTestCase {
+    
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftRepository().text, "Hello, World!")
     }
 
     static var allTests = [
